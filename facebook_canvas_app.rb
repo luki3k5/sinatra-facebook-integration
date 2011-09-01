e4943c4de5180f7d5e73f19cd6097a6dcb7f44ca
luki3k5 <luki3k5@gmail.com>
2011-09-01 17:09:08 +0200

    changing settings and adding configure block

--- facebook_canvas_app.rb
+++ facebook_canvas_app.rb
@@ -7,30 +7,32 @@ require 'facebook_helpers'
 
 set :views, File.dirname(__FILE__) + '/views'
 
-# reading in the config with the details about fb application
-yml = YAML::load(File.open(File.dirname(__FILE__) + "/facebook_app_config.yml"))
+configure do
+  # reading in the config with the details about fb application
+  yml = YAML::load(File.open(File.dirname(__FILE__) + "/facebook_app_config.yml"))
+  
+  set :app_id,     yml["app_id"]
+  set :app_name,   yml["app_name"]
+  set :app_secret, yml["app_secret"]
 
-set APP_ID,     yml["app_id"]
-set APP_NAME,   yml["app_name"]
-set APP_SECRET, yml["app_secret"]
-
-set APP_URL,   "https://apps.facebook.com/#{APP_NAME}/"
-set OAUTH_URL, "https://www.facebook.com/dialog/oauth/"
+  set :app_url,   "https://apps.facebook.com/#{settings.app_name}/"
+  set :oauth_url, "https://www.facebook.com/dialog/oauth/"
+end
 
 # First step is to display page with redirect
 post '/' do
-  if get_current_user
-    @the_url = "#{APP_URL}login"
+  if get_current_user(settings.app_secret)
+    @the_url = "#{settings.app_url}login"
   else
-    @the_url = "#{OAUTH_URL}?client_id=#{@APP_ID}&redirect_uri=#{APP_URL}&scope=email" # HERE IS WHAT WE REQUEST (EMAIL ONLY)
+    @the_url = "#{settings.oauth_url}?client_id=#{settings.app_id}&redirect_uri=#{settings.app_url}&scope=email" # HERE IS WHAT WE REQUEST (EMAIL ONLY)
   end
   erb :index
 end
 
 # Second step is to perform oauth authentycation and obtain data
 post '/login' do
-  oauth_client = OAuth2::Client.new(APP_ID, APP_SECRET, :site => 'https://graph.facebook.com')
-  access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{APP_URL}login")
+  oauth_client = OAuth2::Client.new(settings.app_id, settings.app_secret, :site => 'https://graph.facebook.com')
+  access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}login")
   
   @facebook_user = JSON.parse(access_token.get('/me'))
   erb :details
