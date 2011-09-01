dc1ac546f2e0bee72f8ab35e1ddf953a81966c85
luki3k5 <luki3k5@gmail.com>
2011-09-01 17:42:58 +0200

    fix

--- facebook_canvas_app.rb
+++ facebook_canvas_app.rb
@@ -42,9 +42,10 @@ end
 # Second step is to perform oauth authentycation and obtain data
 post '/login' do
   oauth_client = OAuth2::Client.new(settings.app_id, settings.app_secret, :site => 'https://graph.facebook.com')
-  access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}login")
-  
-  @facebook_user = JSON.parse(access_token.get('/me'))
+  unless params[:code].blank?
+    access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}login")
+    @facebook_user = JSON.parse(access_token.get('/me'))
+  end
   erb :details
 end
 
