5e41d9879d8d3e085ffa7b2825e4a06eecf93bee
luki3k5 <luki3k5@gmail.com>
2011-09-05 12:07:41 +0200

    fix for wrong redirect URL

--- facebook_canvas_app.rb
+++ facebook_canvas_app.rb
@@ -38,7 +38,7 @@ post '/' do
     erb :index
   else
     oauth_client = OAuth2::Client.new(settings.app_id, settings.app_secret, :site => 'https://graph.facebook.com')
-    access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}login")
+    access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}")
     @facebook_user = JSON.parse(access_token.get('/me'))
     erb :details
   end  
