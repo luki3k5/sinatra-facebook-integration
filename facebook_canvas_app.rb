03d908e4fa34e6e556061c8fed1e64fe4eec85de
luki3k5 <luki3k5@gmail.com>
2011-09-05 12:23:09 +0200

    moved OAuth client initi into the configure

--- facebook_canvas_app.rb
+++ facebook_canvas_app.rb
@@ -15,8 +15,9 @@ configure do
   set :app_name,   yml["app_name"]
   set :app_secret, yml["app_secret"]
 
-  set :app_url,   "https://apps.facebook.com/#{settings.app_name}/"
-  set :oauth_url, "https://www.facebook.com/dialog/oauth/"
+  set :app_url,    "https://apps.facebook.com/#{settings.app_name}/"
+  set :oauth_url,  "https://www.facebook.com/dialog/oauth/"
+  set :oauth_client OAuth2::Client.new(settings.app_id, settings.app_secret, :site => 'https://graph.facebook.com')
 end
 
 # helpers do
@@ -39,8 +40,7 @@ post '/' do
     erb :index
   else
     puts "I AM HERE2 : and settings are: #{settings.app_id} #{settings.app_secret} #{settings.app_url} GOING FWD"
-    oauth_client = OAuth2::Client.new(settings.app_id, settings.app_secret, :site => 'https://graph.facebook.com')
-    access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}")
+    access_token = settings.oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}")
     @facebook_user = JSON.parse(access_token.get('/me'))
     erb :details
   end  
