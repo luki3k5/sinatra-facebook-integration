5665dc1585773d72a24fe00d3627c3f6f2b2c291
luki3k5 <luki3k5@gmail.com>
2011-09-05 12:17:17 +0200

    more debug

--- facebook_canvas_app.rb
+++ facebook_canvas_app.rb
@@ -38,7 +38,7 @@ post '/' do
     @the_url = "http://www.facebook.com/dialog/oauth?client_id=#{settings.app_id}&redirect_uri=#{settings.app_url}"
     erb :index
   else
-    puts "I AM HERE2 : and settings are: #{settings.inspect} GOING FWD"
+    puts "I AM HERE2 : and settings are: #{settings.app_id} #{settings.app_secret} #{settings.app_url} GOING FWD"
     oauth_client = OAuth2::Client.new(settings.app_id, settings.app_secret, :site => 'https://graph.facebook.com')
     access_token = oauth_client.web_server.get_access_token(params[:code], :redirect_uri => "#{settings.app_url}")
     @facebook_user = JSON.parse(access_token.get('/me'))
